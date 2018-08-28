1d3cff09e605371e92f08f040c864fd90f01cfb6
Lucas Eriksson <dracc@dracc.se>
2018-08-28 11:53:15 +0200

    Added bash version of the script

---
+++ sym2idc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -f "$1" ]; then
+    echo "Selected input file not found!"
+    exit 2
+fi
+
+SYMBOLLINE=`sed -n '1,$ { /\[Symbols\]/= }' "$1"`
+OUTFILE=$1.idc
+
+echo "#include <idc.idc>
+
+static main()
+{"
+
+sed "1, $SYMBOLLINE d" "$1" | tr -d '$' | sed '/^\s*$/d' | \
+    awk -F ";" '{ print $1}' | \
+    awk -F "=" '{print "      MakeName(0x" $2 ", \"" $1 "\");" }'
+echo "  }"
